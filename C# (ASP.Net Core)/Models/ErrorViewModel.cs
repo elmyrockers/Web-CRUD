18c2024f81d2f8ac189e73c0bcd5f2db2c8789ca
elmyrockers <elmyrockers@gmail.com>
2025-05-12 02:17:09 +0800

    Include generated source code from dotnet command

---
+++ C# (ASP.Net Core)/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace C___ASP.Net_Core_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
