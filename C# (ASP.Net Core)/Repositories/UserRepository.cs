fd58700b5fdd18f7d66652d3c43f716c66732d31
elmyrockers <elmyrockers@gmail.com>
2025-05-15 02:01:15 +0800

    Add UserRepository to manipulate users table

---
+++ C# (ASP.Net Core)/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using WebCRUD.Models;
+using Dumpify;
+
+namespace WebCRUD.Repositories;
+
+public class UserRepository
+{
+	private readonly IDbConnection _db;
+
+	public UserRepository(IDbConnection db)
+	{
+		_db = db;
+	}
+
+	public async Task<IEnumerable<UserModel>> GetAll()
+	{
+		var sql = @"SELECT *,
+						   created_at AS CreatedAt,
+						   updated_at AS UpdatedAt
+					FROM users";
+		return await _db.QueryAsync<UserModel>(sql);
+	}
+}
