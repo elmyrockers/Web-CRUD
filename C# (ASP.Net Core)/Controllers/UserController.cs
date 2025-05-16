24d97e3b46c9c74df4ccd3b148f1d8ba28ece36e
elmyrockers <elmyrockers@gmail.com>
2025-05-16 19:50:31 +0800

    Add new action: UserController::New(), then update List() with flash message

--- C# (ASP.Net Core)/Controllers/UserController.cs
+++ C# (ASP.Net Core)/Controllers/UserController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using WebCRUD.Repositories;
+using WebCRUD.DTOs;
+using WebCRUD.Models;
 using Dumpify;
+using System;
 
 namespace WebCRUD.Controllers;
 
@@ -17,15 +21,18 @@ public class UserController : Controller
 	[HttpGet]
 	public IActionResult Index()
 	{
-		return Redirect( "/users" );
+		return LocalRedirect( "/users" );
 	}
 
 	[HttpGet]
-	public IActionResult List()
+	public async Task<IActionResult> List()
 	{
-		var users = _userRepository.GetAll();
-		users.Dump();
-		return View();
+		var users = await _userRepository.GetAll();
+		string flashMessage = HttpContext.Session.GetString( "flash_message" );
+		HttpContext.Session.Remove( "flash_message" );
+
+		ViewBag.Flash = flashMessage;
+		return View( users );
 	}
 
 	[HttpGet]
@@ -44,5 +51,28 @@ public class UserController : Controller
 	public IActionResult Delete()
 	{
 		return View();
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> New(NewUserRequest request)
+	{
+		if ( !ModelState.IsValid ) return BadRequest( ModelState );
+
+		// Insert form data into 'users' table
+			var user = new UserModel
+			{
+				Name = request.Name,
+				Email = request.Email,
+				Website = request.Website
+			};
+			var insertID = await _userRepository.Insert( user );
+
+		// Set flash message then redirect to 'users' table
+			if( insertID <= 0 ){
+				HttpContext.Session.SetString( "flash_message", "<div class='alert alert-danger'>Failed to add new user!</div>" );
+				return LocalRedirect( "/users" );
+			}
+			HttpContext.Session.SetString( "flash_message", "<div class='alert alert-success'>The new user has been added successfully!</div>" );
+			return LocalRedirect( "/users" );
 	}
 }
